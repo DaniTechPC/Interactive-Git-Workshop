e25604c9d91a08bd4768ae7e9e8060bf4d149d7a
Matthew Reese <awesomepop012@hotmail.com>
2026-02-26 17:02:04 -0800

    Add barrel role functionality

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function showProjects() {
         list.style.display = "none";
     }
 }
+
+// Credit to https://stackoverflow.com/a/8000238 and https://stackoverflow.com/a/8175834
+var A = 0;
+function doABarrelRoll() {
+    c = document.createElement("style");
+    c.innerHTML = `@-moz-keyframes roll` + A + ` {
+    from { -moz-transform: rotate(` + A + `deg) }
+    to   { -moz-transform: rotate(` + (A + 360) +`deg) }
+}
+body {
+    -moz-animation-name: roll` + A + `;
+    -moz-animation-duration: 4s;
+    -moz-animation-iteration-count: 1;
+   }`;
+   A += 360;
+    document.head.appendChild(c);
+}
