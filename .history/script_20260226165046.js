7bf32f58a3068e08ad30decb054a069400c80e8f
Brandonhaaas <127485183+Brandonhaaas@users.noreply.github.com>
2026-02-26 17:56:42 -0800

    add something new to the website

--- .history/script_20260226165046.js
+++
@@ -1,9 +0,0 @@
-function showProjects() {
-    let list = document.getElementById("project-list")
-
-    if (list.style.display === "none") {
-        list.style.display = "block"
-    } else {
-        list.style.display = "none"; // BUG: should be string
-    }
-}
